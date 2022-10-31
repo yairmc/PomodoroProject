23d21e47b16c19e797eb6534f5655be512243613
yairmc <yairmc666@gmail.com>
2022-10-31 15:36:06 -0700

    Creando la funcionalidad de agregar Tarea

--- index.js
+++ index.js
@@ -2,6 +2,38 @@ const btnAddModal = document.querySelector("#addButton");
 const btnCloseAddModal = document.querySelector('#closeModal');
 const modal = document.querySelector('#modal')
 
+const form = document.querySelector('#form');
+const inputNameTask = document.querySelector('#inputNameTask');
+const inputDescriptionTask = document.querySelector('#inputDescriptionTask');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const taskName = inputNameTask.value;
+    const descriptionTask=inputDescriptionTask.value;
+
+    if (!taskName) return alert("Porfavor ingresa el nombre de la tarea")
+
+    const pendingClass = document.querySelector('#pendingClass');
+
+    const pendingTask = document.createElement('div');
+    pendingTask.classList.add('pendingTask');
+
+    pendingClass.appendChild(pendingTask);
+
+
+    const pendingTaskName = document.createElement('h3');
+    pendingTaskName.classList.add('pendingNameTask');
+    pendingTaskName.innerText=taskName
+
+    const pendingTaskDescription = document.createElement('h3');
+    pendingTaskDescription.classList.add('pendingDescriptionTask');
+    pendingTaskDescription.innerText=descriptionTask
+    
+    pendingTask.appendChild(pendingTaskName)
+    pendingTask.appendChild(pendingTaskDescription)
+})
+
 btnAddModal.addEventListener("click", () => {
     modal.showModal()
 })
