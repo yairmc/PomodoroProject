819b601db23998b761e774d8eb5de79e6b3dbd68
yairmc <yairmc666@gmail.com>
2022-10-31 19:32:58 -0700

    vlidations in the inputs

--- index.js
+++ index.js
@@ -2,42 +2,67 @@ const btnAddModal = document.querySelector("#addButton");
 const btnCloseAddModal = document.querySelector('#closeModal');
 const modal = document.querySelector('#modal')
 
+let listItems = localStorage.getItem('myTodoList') ? JSON.parse(localStorage.getItem('myTodoList')) : [];
+
 const form = document.querySelector('#form');
 const inputNameTask = document.querySelector('#inputNameTask');
 const inputDescriptionTask = document.querySelector('#inputDescriptionTask');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+window.addEventListener('load', () => {
+    loadPendingTask(listItems)
 
-    const taskName = inputNameTask.value;
-    const descriptionTask=inputDescriptionTask.value;
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
 
-    if (!taskName) return alert("Porfavor ingresa el nombre de la tarea")
+        const nameTask = inputNameTask.value;
+        const descriptionTask = inputDescriptionTask.value;
 
+        for (let i = 0; listItems.length > i; i++) {
+            if (nameTask === listItems[i].name) {
+                alert('El nombre ya esta en la lista: ' + listItems[i].name);
+                return;
+            } else if (descriptionTask === listItems[i].description) {
+                alert(' La descripcion ya esta en la lista: ' + listItems[i].description)
+                return;
+            }
+        }
+        if (nameTask == null || descriptionTask == null) {
+            return
+        } else {
+            modal.showModal()
+            listItems.push({ name: nameTask, description: descriptionTask, id: `task-${Math.floor(Math.random() * 300)}` });
+            localStorage.setItem('myTodoList', JSON.stringify(listItems));
+        }
+
+        loadPendingTask(listItems)
+        inputNameTask.value = '';
+        inputDescriptionTask.value = '';
+        btnCloseAddModal.addEventListener("click", () => {
+            modal.close()
+        })
+    })
+})
+
+const loadPendingTask = (todoPending) => {
     const pendingClass = document.querySelector('#pendingClass');
 
-    const pendingTask = document.createElement('div');
-    pendingTask.classList.add('pendingTask');
+    pendingClass.innerHTML = "";
+    todoPending.forEach(item => {
+        const pendingTask = document.createElement('div');
+        pendingTask.classList.add('pendingTask');
 
-    pendingClass.appendChild(pendingTask);
+        pendingClass.appendChild(pendingTask);
 
 
-    const pendingTaskName = document.createElement('h3');
-    pendingTaskName.classList.add('pendingNameTask');
-    pendingTaskName.innerText=taskName
+        const pendingTaskName = document.createElement('h3');
+        pendingTaskName.classList.add('pendingNameTask');
+        pendingTaskName.innerText = item.name;
 
-    const pendingTaskDescription = document.createElement('h3');
-    pendingTaskDescription.classList.add('pendingDescriptionTask');
-    pendingTaskDescription.innerText=descriptionTask
-    
-    pendingTask.appendChild(pendingTaskName)
-    pendingTask.appendChild(pendingTaskDescription)
-})
+        const pendingTaskDescription = document.createElement('h3');
+        pendingTaskDescription.classList.add('pendingDescriptionTask');
+        pendingTaskDescription.innerText = item.description
 
-btnAddModal.addEventListener("click", () => {
-    modal.showModal()
-})
-
-btnCloseAddModal.addEventListener("click", () => {
-    modal.close()
-})
+        pendingTask.appendChild(pendingTaskName)
+        pendingTask.appendChild(pendingTaskDescription)
+    })
+}
