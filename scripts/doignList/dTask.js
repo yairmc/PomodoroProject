dae311953d604aee118ffe793ef8b15ada775c80
yairmc <yairmc666@gmail.com>
2022-11-07 21:11:05 -0700

    pomodoro its works

--- scripts/doignList/dTask.js
+++ scripts/doignList/dTask.js
@@ -1,5 +1,5 @@
 
-export default function dTask(item){
+export default function dTask(item) {
     const doingClass = document.querySelector('#doingClass');
 
     doingClass.innerHTML += "";
@@ -18,11 +18,81 @@ export default function dTask(item){
     doingTaskDescription.classList.add('doingDescriptionTask');
     doingTaskDescription.innerText = item.description
 
-    const doButtonTaskDescription = document.createElement('div');
-    doButtonTaskDescription.classList.add('doButton');
-    doButtonTaskDescription.innerText = "Temporisador"
+    // timer 
+    const panel = document.createElement('div')
+    panel.classList.add('timeContainer')
+
+    const start = document.createElement('p');
+    start.classList.add('start');
+    start.innerText = "Empezar"
+
+    const stop = document.createElement('p')
+    stop.classList.add('stop');
+    stop.innerText = "detenerse"
+
+    const timer = document.createElement('div');
+    timer.classList.add('timer');
+
+    const minutes = document.createElement('p');
+    minutes.setAttribute('id', 'minutes');
+
+    const seconds = document.createElement('p')
+    seconds.setAttribute('id', 'seconds')
 
     doingTask.appendChild(doingTaskName)
     doingTask.appendChild(doingTaskDescription)
-    doingTask.appendChild(doButtonTaskDescription)
+
+    doingTask.appendChild(panel)
+    panel.appendChild(start)
+    panel.appendChild(stop)
+
+    doingTask.appendChild(timer)
+    timer.appendChild(minutes);
+    timer.appendChild(seconds);
+
+    let workTime = 1;
+    let restTime = 5;
+    let secondsTime = '00';
+
+    minutes.innerHTML = workTime
+    seconds.innerHTML = secondsTime
+
+    start.addEventListener('click', (e) => {
+        console.log('hiciste click');
+        startTimer();
+    })
+
+    function startTimer(){
+        secondsTime = 59;
+        let workMinutes = workTime - 1;
+        let restMinutes = restTime - 1;
+
+        let breakCount = 0;
+
+        let timeFunction = () => {
+            minutes.innerHTML = workMinutes;
+            seconds.innerHTML = secondsTime;
+
+            secondsTime=secondsTime-1;
+
+            if(secondsTime===0){
+                workMinutes=workMinutes-1;
+                if(workMinutes===-1){
+                    if(breakCount%2===0){
+                        workMinutes=restMinutes;
+                        breakCount++;
+                    }else{
+                        workMinutes=workTime;
+                        breakCount++;
+                    }
+                }
+                secondsTime=59;
+            }
+        }
+        setInterval(timeFunction, 1000)
+
+    }
+
 }
+
+
